3132ed887deaca9338aca431a55bf8ad4258dbff
Yaneart <artemskek@gmail.com>
2026-08-04 09:53:39 +0300

    app colors

--- frontend/src/App.tsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <div className="text-8xl text-green-800">yaneMedia</div>
-  )
-}
-
-export default App;
---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div>yaneMedia</div>
+  )
+}
+
+export default App;
