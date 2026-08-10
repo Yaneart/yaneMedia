6da5a9ff12acfab06f4638b39000570e83800dde
Yaneart <artemskek@gmail.com>
2026-08-10 10:07:27 +0300

    add app routes

---
+++ frontend/src/app/router/routes.ts
@@ -0,0 +1,13 @@
+export const routePaths = {
+  home: '/',
+  search: '/search',
+  movies: '/movies',
+  series: '/series',
+  anime: '/anime',
+  media: '/media/:mediaId',
+  favorites: '/favorites',
+  history: '/history',
+  login: '/login',
+  register: '/register',
+  notFound: '*',
+} as const;
