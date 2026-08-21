c45069777e904893688440049ec25d3f9e7b6bca
Yaneart <artemskek@gmail.com>
2026-08-21 11:51:43 +0300

    allow frontend development origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,10 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api/v1');
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
+
   const port = configService.getOrThrow<number>('PORT');
 
   await app.listen(port);
