7aa261d22ca056a883a7402fd876d582e89aa23b
Yaneart <artemskek@gmail.com>
2026-08-21 10:32:59 +0300

    validate backend environment

--- backend/src/config/config.module.ts
+++ backend/src/config/config.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       cache: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('development', 'test', 'production').default('development'),
+        PORT: Joi.number().port().default(3000),
+      }),
     }),
   ],
   exports: [ConfigModule],
