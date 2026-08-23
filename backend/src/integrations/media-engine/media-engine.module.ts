5e033a8db70f0ec4a07130d1f6ffe2ca2cabfac3
Yaneart <artemskek@gmail.com>
2026-08-24 00:09:20 +0300

    extract media engine factory

--- backend/src/integrations/media-engine/media-engine.module.ts
+++ backend/src/integrations/media-engine/media-engine.module.ts
@@ -2,20 +2,22 @@ import { Module } from '@nestjs/common';
 
 export const MEDIA_ENGINE = Symbol('MEDIA_ENGINE');
 
+async function createMediaEngine() {
+  const [{ MediaEngine }, { cinemetaProvider, kinobdProvider }] = await Promise.all([
+    import('@media-engine/core'),
+    import('@media-engine/providers'),
+  ]);
+
+  return new MediaEngine({
+    providers: [kinobdProvider(), cinemetaProvider()],
+  });
+}
+
 @Module({
   providers: [
     {
       provide: MEDIA_ENGINE,
-      useFactory: async () => {
-        const [{ MediaEngine }, { cinemetaProvider, kinobdProvider }] = await Promise.all([
-          import('@media-engine/core'),
-          import('@media-engine/providers'),
-        ]);
-
-        return new MediaEngine({
-          providers: [kinobdProvider(), cinemetaProvider()],
-        });
-      },
+      useFactory: createMediaEngine,
     },
   ],
   exports: [MEDIA_ENGINE],
